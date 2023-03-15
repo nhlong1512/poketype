29341732a0655c064cb9dc7712e5984c124ad943
nhlong1512 <20521569@gm.uit.edu.vn>
2023-03-15 11:48:39 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Button, Space } from 'antd';
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
+import axios from "axios";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<string>("")
   return (
-    <div className="App">
-      Hello World
+    <div className="App m-0 p-0">
+      <Layout className="h-[100vh] m-0 bg-[#3d405b]">
+        <Content className="my-[16px] mx-[32px]">
+          <header className="text-[#81b29a] tracking-[0.25rem] font-[600] text-[40px] text-center">
+            Pokemon
+          </header>
+        </Content>
+      </Layout>
     </div>
-  )
-}
+  );
+};
 
 export default App;
